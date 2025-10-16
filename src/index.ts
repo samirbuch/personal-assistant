462214bb3621051346a54d57ea3a0820d2859f3f
bdotsamir <samir.js.zip@gmail.com>
2025-10-16 15:12:52 -0400

    index: call media streams are now forwarded to the websocket!

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,97 @@
-import twilio from "twilio";
+import Twilio from "twilio";
 import * as Bun from "bun";
+import cleanPublicURL from "./util/cleanPublicURL";
+import { TwilioWebsocket } from "../lib/TwilioWebsocketTypes";
 
 const PORT = process.env.PORT || 40451
 
 console.log("Hello via Bun!");
 
-const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+const client = Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 Bun.serve({
   port: PORT,
   routes: {
-    "/api/twilio-gateway": async req => {
-      let publicURL = process.env.PUBLIC_URL;
-      if (!publicURL) return new Response("Error 500: Missing PUBLIC_URL", { status: 500 });
-
-      if (/^https?:\/\//.test(publicURL)) {
-        // Remove a http:// or https://
-        publicURL = publicURL.split(/^https?:\/\//)[1];
-        if (!publicURL) return new Response("Error 500: Malformed PUBLIC_URL", { status: 500 });
+    "/api/calls/:number": async req => {
+      if (req.method !== "POST") {
+        return new Response("Error 405: Method not allowed", { status: 405 });
       }
+
+      const from = process.env.TWILIO_PHONE_NUMBER;
+      if (!from) return new Response("Error 500: Missing TWILIO_PHONE_NUMBER", { status: 500 });
+
+      const publicURL = cleanPublicURL(process.env.PUBLIC_URL);
+      if (!publicURL) return new Response("Error 500: Missing or malformed PUBLIC_URL", { status: 500 });
 
       const isNgrok = publicURL.includes(".ngrok-free.app");
-      if (isNgrok && publicURL.includes(":")) {
-        console.warn("PUBLIC_URL contains a PORT. This should be automatically forwarded by ngrok.");
-        console.warn("Cleaning URL...");
-        const portMatch = publicURL.match(/:\d{1,5}/)![0];
-        const indexOfPort = publicURL.indexOf(portMatch);
-        publicURL = publicURL.slice(0, indexOfPort) + publicURL.slice(indexOfPort + portMatch.length, publicURL.length);
-      }
+      const url = `http://${publicURL}${isNgrok ? "" : `:${PORT}`}/api/twilio-gateway`;
 
-      return new Response(`ws://${publicURL}${isNgrok ? "" : `:${PORT}`}/twilio-ws`);
+      console.log(`Calling ${from}`);
+      const call = await client.calls.create({
+        from,
+        to: req.params.number,
+        url: url
+      });
+
+      console.log("Call!", call);
+
+      return new Response(JSON.stringify(call.toJSON()));
+    },
+    "/api/twilio-gateway": async req => {
+      const publicURL = cleanPublicURL(process.env.PUBLIC_URL);
+      if (!publicURL) return new Response("Error 500: Missing or malformed PUBLIC_URL", { status: 500 });
+
+      const isNgrok = publicURL.includes(".ngrok-free.app");
+      const ws = `wss://${publicURL}${isNgrok ? "" : `:${PORT}`}/twilio-ws`;
+
+      const response = new Twilio.twiml.VoiceResponse();
+      const start = response.start();
+      start.stream({
+        name: "Inbound Audio Stream",
+        url: ws
+      });
+      response.say("Hello world!");
+      console.log("Forwarding twilio call stream to:", ws);
+
+      return new Response(
+        response.toString(),
+        {
+          headers: {
+            "Content-Type": "text/xml"
+          }
+        }
+      );
     }
   },
 
   fetch(req, server) {
     const url = new URL(req.url);
+    console.log(url);
     if (url.pathname === "/twilio-ws") {
       const upgraded = server.upgrade(req);
       if (!upgraded) {
         return new Response("Upgrade failed", { status: 400 });
       }
     }
-    return new Response("Hello World");
+    // return new Response("Hello World");
   },
   websocket: {
+    open(ws) {
+      console.log("Websocket connection opened!");
+    },
     message(ws, message) {
+      console.log("New message:", message);
+      const json: TwilioWebsocket.Message = JSON.parse(message as string);
 
+      // Echo back to the caller
+      if(json.event === "media") {
+        const send: TwilioWebsocket.Sendable.MediaMessage = {
+          streamSid: json.streamSid,
+          event: "media",
+          media: { payload: json.media.payload }
+        }
+        ws.send(JSON.stringify(send))
+      }
     }
   }
 })
