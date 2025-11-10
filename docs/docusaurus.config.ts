72f44bf18b12da95d72aa67995d25eac1fdc792c
bdotsamir <samir.js.zip@gmail.com>
2025-11-10 12:18:54 -0500

    Merge docs from classroom/main
    Co-authored-by: github-classroom[bot] <66690702+github-classroom[bot]@users.noreply.github.com>
    Co-authored-by: Ian Applebaum <Ian.tyler@electricbluefish.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -2,10 +2,42 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+const main_template_jira_scripts = () => {
+  if (process.env.ORG_NAME === 'ApplebaumIan'){
+    return [    'https://temple-cis-projects-in-cs.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/azc3hx/b/8/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=50af7ec2',
+      'https://temple-cis-projects-in-cs.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/azc3hx/b/8/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=160e88a6',]
+  } else {
+    return []
+  }
+}
+
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
+/**
+ * The URL or reference to your projects logo!
+ */
+const logo = 'https://upload.wikimedia.org/wikipedia/commons/1/17/Temple_T_logo.svg';
+
+// Fallback value if PROJECT_NAME is not defined:
+const rawProjectName = process.env.PROJECT_NAME || 'personal-assistant';
+
+// Transform PROJECT_NAME (or fallback) to a title-like string:
+const title = rawProjectName
+  .replace(/-/g, ' ')
+  .split(' ')
+  .map(word => {
+    // Make sure the word has at least one character
+    return word.length > 0
+      ? word[0].toUpperCase() + word.substring(1)
+      : '';
+  })
+  .join(' ');
+
+const baseUrl = process.env.PROJECT_NAME || "personal-assistant";
+const orgName = process.env.ORG_NAME || 'samirbuch';
+
 const config: Config = {
-  title: 'Personal Assistant',
+  title: title,
   tagline: 'Who needs Jarvis?',
   favicon: 'img/favicon.ico',
 
@@ -23,17 +55,19 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: `https://${orgName}.github.io/`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: `/${baseUrl}/`,
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'samirbuch', // Usually your GitHub org/user name.
-  projectName: 'personal-assistant', // Usually your repo name.
+  organizationName: orgName, // Usually your GitHub org/user name.
+  projectName: process.env.PROJECT_NAME || 'personal-assistant', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -48,45 +82,61 @@ const config: Config = {
       'classic',
       {
         docs: {
+          showLastUpdateAuthor: true,
           sidebarPath: './sidebars.ts',
+          routeBasePath: 'docs',
+          path: 'docs',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            `https://github.com/${orgName}/${process.env.PROJECT_NAME || 'personal-assistant'}/edit/main/docs/`,
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   feedOptions: {
-        //     type: ['rss', 'atom'],
-        //     xslt: true,
-        //   },
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        //   // Useful options to enforce blogging best practices
-        //   onInlineTags: 'warn',
-        //   onInlineAuthors: 'warn',
-        //   onUntruncatedBlogPosts: 'warn',
-        // },
         theme: {
           customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
+    [
+      'redocusaurus',
+      {
+        // Plugin Options for loading OpenAPI files
+        specs: [
+          {
+            id: 'using-single-yaml',
+            spec: 'static/openapi.yml.yaml',
+            route: '/api/',
+          },
+        ],
+        // Theme Options for modifying how redoc renders them
+        theme: {
+          // Change with your site colors
+          customCss: './src/css/custom.css',
+        },
+      },
+    ] as any,
   ],
 
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    ...(process.env.NODE_ENV === 'development' ? {
+      announcementBar: {
+        id: 'dev_mode',
+        content:
+          'You are currently working on a local development version of your docs. This is <b>NOT</b> the live site.',
+        backgroundColor: '#ffca00',
+        textColor: '#091E42',
+        isCloseable: false,
+      }
+    } : {}),
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: title,
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: logo,
       },
       items: [
         {
@@ -97,7 +147,7 @@ const config: Config = {
         },
         // { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: `https://github.com/${orgName}/${process.env.PROJECT_NAME || 'personal-assistant'}`,
           label: 'GitHub',
           position: 'right',
         },
@@ -105,54 +155,75 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
+      logo: {
+        alt: 'My Site Logo',
+        src: logo,
+      },
       links: [
         {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Documentation',
               to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
             },
           ],
         },
         {
           title: 'More',
           items: [
-            // {
-            //   label: 'Blog',
-            //   to: '/blog',
-            // },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: `https://github.com/${orgName}/${process.env.PROJECT_NAME || 'personal-assistant'}`,
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} ${title}, Inc. Built with Docusaurus.`,
+    },
+    imageZoom: {
+      // CSS selector to apply the plugin to, defaults to '.markdown img'
+      selector: '.markdown img',
+      // Optional medium-zoom options
+      // see: https://www.npmjs.com/package/medium-zoom#options
+      options: {
+        margin: 24,
+        zIndex: 100,
+        background: 'white',
+        // scrollOffset: 10,
+        // container: '#zoom-container',
+        // template: '#zoom-template',
+      },
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  plugins: [
+    [
+      "docusaurus2-dotenv-2",
+      {
+        systemvars: true,
+      },
+    ],
+    'plugin-image-zoom',
+    ...(process.env.ORG_NAME !== 'ApplebaumIan' ? [[
+      'docusaurus-plugin-remote-content',
+      {
+        name: 'open-source-usage',
+        sourceBaseUrl: 'https://applebaumian.github.io/tu-cis-4398-docs-template/',
+        outDir: 'tutorial',
+        documents: ['open-source-usage.mdx'],
+      },
+    ]] : [])
+  ],
+
+  scripts: [
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js',
+    ...main_template_jira_scripts()
+  ],
 };
 
 export default config;
--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -82,13 +82,6 @@ const config = {
           // remarkPlugins: [require('mdx-mermaid')],
 
         },
-        // tutorials: {
-        //   sidebarPath: require.resolve('./tutorialSidebars.js'),
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
 
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -115,7 +108,6 @@ const config = {
     ],
   ],
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       ...(process.env.NODE_ENV === 'development' ? {announcementBar : {
         id: 'dev_mode',
@@ -166,23 +158,6 @@ const config = {
               },
             ],
           },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
           {
             title: 'More',
             items: [
