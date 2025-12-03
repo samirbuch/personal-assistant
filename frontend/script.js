68345ba9788d7c8f324546341669cc2f3b61e895
dagt03 <dgalvezterrones@gmail.com>
2025-12-03 11:51:43 -0500

    Add HTML + CSS for appointments table

--- frontend/script.js
+++ frontend/script.js
@@ -1,19 +1,36 @@
-// Import Supabase via CDN (if using type="module" in HTML)
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// Replace these with your Supabase project info
-const supabaseUrl = 'https://zyxlbcypsbyrounrypsy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5eGxiY3lwc2J5cm91bnJ5cHN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4Nzg5NTMsImV4cCI6MjA3ODQ1NDk1M30.1RyEMa9tCiWMWCH0EDN4uGQ5dS_mcmBhUFekuSeGfWw';
 
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function fetchTest() {
-    const { data, error } = await supabase.from("test").select("*");
-    if (error) {
-        console.error('Error fetching data:', error);
-    } else {
-        console.log('Data:', data);
+let endtime = document.getElementById("end-time");
+let starttime = document.getElementById("start-time");
+
+function startBeforeEnd(start, end) {
+    let [startHours, startMinutes] = start.split(":").map(Number);
+    let [endHours, endMinutes] = end.split(":").map(Number);
+
+    console.log(`Start - Hours: ${startHours}, Minutes: ${startMinutes}`);
+    console.log(`End - Hours: ${endHours}, Minutes: ${endMinutes}`);
+
+    if(startHours > endHours || (startHours === endHours && startMinutes > endMinutes)) {
+        return false; // Start time is later than end time
     }
+    return true; // Start time is earlier than or equal to end time
 }
 
-fetchTest();
+
+
+starttime.addEventListener("change", () => {
+    if(!startBeforeEnd(starttime.value, endtime.value)){
+        endtime.value = starttime.value;
+        endtime.min = starttime.value;
+    }
+})
+
+endtime.addEventListener("change", () => {
+    if(!startBeforeEnd(starttime.value, endtime.value)){
+        endtime.value = starttime.value;
+        endtime.min = starttime.value;
+        let end = document.querySelector(".form-entry end");
+        console.log(end)
+    }
+})
